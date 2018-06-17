630852a2bd2f7990a168fff238ca5fa18b3a0ad9
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 15:23:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,4 +20,7 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
